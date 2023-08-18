cead352de47d90f0faa9abe44157b4eb669e79f3
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-18 09:42:10 +0300

    npm init

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './style/reset.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <App />
+  <App />,
   // </React.StrictMode>,
-)
+);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,14 @@
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to Home Page</h1>
+    <div className="container">
+      <h1 className="text-3xl font-bold underline">Welcome to Home Page</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
         quaerat incidunt quo totam fugit enim a quidem suscipit error porro odio
         obcaecati voluptatem dolorem, quas dolorum exercitationem natus dolore
         esse.
       </p>
+      <button>Click me</button>
     </div>
   );
 }
