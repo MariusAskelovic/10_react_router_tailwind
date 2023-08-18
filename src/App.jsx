86b3ac970b369409aa60288d644888f2c6d9d3bf
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-18 09:52:25 +0300

    activePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
+import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
 
 export default function App() {
+  const [activePage, setActivePage] = useState(1);
+
   return (
     <div>
-      <HomePage />
+      <div>
+        <button
+          onClick={() => {
+            setActivePage(1);
+          }}
+          className="border hover:bg-slate-400 border-cyan-600 p-3 rounded-sm px-3 py-1"
+        >
+          Home
+        </button>
+        <button
+          onClick={() => {
+            setActivePage(2);
+          }}
+          className="border hover:bg-slate-400 border-cyan-600 p-3 borderrounder-sm px-3 py-1"
+        >
+          About
+        </button>
+      </div>
+      {activePage === 1 && <HomePage />}
+      {activePage === 2 && <AboutPage />}
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 export default function HomePage() {
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold underline">Welcome to Home Page</h1>
-      <p>
+      <h1 className="text-3xl font-bold underline pb-4">
+        Welcome to Home Page
+      </h1>
+      <p className="">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
         quaerat incidunt quo totam fugit enim a quidem suscipit error porro odio
         obcaecati voluptatem dolorem, quas dolorum exercitationem natus dolore
