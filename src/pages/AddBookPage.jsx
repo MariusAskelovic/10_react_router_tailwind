ada728e56dfb69534de2b9330c46951a73c2de42
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-21 15:40:46 +0300

    validation fully working

--- src/pages/AddBookPage.jsx
+++ src/pages/AddBookPage.jsx
@@ -12,8 +12,11 @@ export default function AddBookPage() {
     validationSchema: Yup.object({
       title: Yup.string().required('required').max(15, 'requires 15 symbols'),
       author: Yup.string().required('required').max(15, 'requires 15 symbols'),
-      year: Yup.string().min(1).max(20).required('required'),
-      genre: Yup.string().required('required').max(15, 'requires 15 symbols'),
+      year: Yup.number()
+        .min(1000, 'enter year between 1000 and 2024')
+        .max(2024, 'enter year between 1000 and 2024')
+        .required('required'),
+      genre: Yup.string().required('required'),
     }),
     onSubmit: (values) => {
       console.log(values);
@@ -25,6 +28,7 @@ export default function AddBookPage() {
       <h1 className="text-3xl font-bold underline pb-4">Add book</h1>
 
       <form className="grid gap-2 justify-start" onSubmit={formik.handleSubmit}>
+        {/* title */}
         <div>
           <input
             onChange={formik.handleChange}
@@ -33,11 +37,13 @@ export default function AddBookPage() {
             className="block py-1 px-2 border border-slate-500"
             type="text"
             placeholder="title"
+            onBlur={formik.handleBlur}
           />
-          {formik.errors.title ? (
-            <p className="text-red-800">{formik.errors.title}</p>
+          {formik.touched.title && formik.errors.title ? (
+            <p className="text-amber-400">{formik.errors.title}</p>
           ) : null}
         </div>
+        {/* author */}
         <div>
           <input
             onChange={formik.handleChange}
@@ -46,29 +52,40 @@ export default function AddBookPage() {
             className="block py-1 px-2 border border-slate-500"
             type="text"
             placeholder="author"
+            onBlur={formik.handleBlur}
           />
-          {formik.errors.author ? (
-            <p className="text-red-800">{formik.errors.author}</p>
+          {formik.touched.author && formik.errors.author ? (
+            <p className="text-blue-800">{formik.errors.author}</p>
           ) : null}
         </div>
+        {/* year */}
         <div>
           <input
             className="block py-1 px-2 border border-slate-500"
             type="text"
             placeholder="year"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.year}
+            id="year"
           />
-          {formik.errors.year ? (
-            <p className="text-red-800">{formik.errors.year}</p>
+          {formik.touched.year && formik.errors.year ? (
+            <p className="text-green-800">{formik.errors.year}</p>
           ) : null}
         </div>
+        {/* genre */}
         <div>
           <input
             className="block py-1 px-2 border border-slate-500"
             type="text"
             placeholder="genre"
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+            value={formik.values.genre}
+            id="genre"
           />
-          {formik.errors.genre ? (
-            <p className="text-red-800">{formik.errors.genre}</p>
+          {formik.touched.genre && formik.errors.genre ? (
+            <p className="text-red-400">{formik.errors.genre}</p>
           ) : null}
         </div>
         {/* <div>
